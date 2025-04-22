ca5d345b1f4b5014784f64d298207280416b20df
lucas02-pixel <lucasss02122014@gmail.com>
2025-04-22 11:03:51 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+let resposta;
+let arroz;
+let qualquercoisa;
+let deutrabalho;
+let doido;
+let pensamentos;
+let irritaçao;
+let escolha;
+let tenta;
+alert ('arroz');
+prompt ('esta achando o jogo chato???');
+alert ('não ligo para sua opiniao');
+alert ('desculpa');
+prompt ('aceita desculpas');
+alert ('não ligo');
+arroz = prompt ('da uma nota de 0 a 10 para o jogo');
+alert (`a sua nota foi ${arroz}`);
+prompt ('acertei?????');
+alert ('não ligo.');
+resposta = prompt ('vc acha que este jogo foi uma boa ideia?????');
+alert (`eu tambem acho que ${resposta}`);
+alert ('eu consigo ler o q voce digita é serio');
+qualquercoisa = prompt ('pra textar digita qualquer coisa')
+alert (`voce digitou ${qualquercoisa}`)
+alert ('viu adivinhei')
+prompt ('aproveite este jogo porque deu trabalho fazer')
+deutrabalho = prompt ('vc acha q deu trabalho???');
+alert (`vc falou que ${deutrabalho}`)
+prompt ('é serio qual é seu nome??????')
+alert ('não ligo')
+prompt ('aceita desculpas pelo o q eu falei')
+alert ('não ligo')
+prompt ('eu sou mais inteligente q vc')
+prompt ('se vc duvidar entao me diz quantos numeros ja descubertos no pi ate hoje no recorde mundial')
+alert ('62,8 trilhões')
+alert ('viu eu sou mais inteligente q vc')
+prompt ('qual é sua comida preferida?????')
+alert('nao ligo')
+doido = prompt ('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaalecrimm dourado')
+alert (`sua reação ${doido}`)
+alert ('kkkkkkkkkkkkkkkkkkk')
+prompt ('ta bravinha é')
+alert ('eu sou muito mais inteligente q vc')
+prompt ('defendasse')
+alert ('kkkkkkkkkkk')
+prompt ('e agora o q vai fazer quebrar o computador')
+alert ('kkkkkkkkk')
+prompt ('como é menino em italiano')
+alert ('eu sei é ragazzo')
+alert ('kkkkkkkkk')
+pensamentos = prompt ('e agora ta gostando do jogo')
+alert (`eu tambem digitaria isto: ${pensamentos}`)
+irritaçao = prompt ('kkkkkkkkkkkkkkkk vc é mt engraçada o q sera q vai responder')
+alert (`respondeu isto: ${irritaçao}`)
+escolha = prompt ('e ai acha o jogo engraçado ou irritante')
+alert (`repondeu ${escolha}`)
+tenta = prompt ('eu quero saber se vc consegue me impressionar e ai tenta')
+alert (`${tenta} nao  me impressiona kkkkk`)
